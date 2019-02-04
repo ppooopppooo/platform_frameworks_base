3fe66fd920b7250ef65cc3fd0fba2ef6f01f8cb2
Anushek Prasal <anushekprasal@gmail.com>
2019-02-04 19:59:50 +0530

    base: QS header improvements
    - Fix the gradient behind the qs
    - Fix the qs panel layout
    - Detach the header image from the top
    - Set the bottom margin only when custom header enabled
    
    Signed-off-by: ppooopppooo <ppooopppooo@qq.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -149,6 +149,9 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     private BatteryMeterView mBatteryRemainingIcon;
     private boolean mPermissionsHubEnabled;
 
+    private boolean mLandscape;
+    private boolean mHeaderImageEnabled;
+    
     private PrivacyItemController mPrivacyItemController;
 
     protected ContentResolver mContentResolver;
@@ -166,25 +169,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.STATUS_BAR_BATTERY_STYLE), false,
                     this, UserHandle.USER_ALL);
-            }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            updateSettings();
-        }
-    }
-    private SettingsObserver mSettingsObserver = new SettingsObserver(mHandler);
-
-    private boolean mLandscape;
-    private boolean mHeaderImageEnabled;
-
-    private class OmniSettingsObserver extends ContentObserver {
-        OmniSettingsObserver(Handler handler) {
-            super(handler);
-        }
-
-        void observe() {
-            ContentResolver resolver = getContext().getContentResolver();
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER), false,
                     this, UserHandle.USER_ALL);
@@ -196,7 +180,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         }
     }
 
-    private OmniSettingsObserver mOmniSettingsObserver = new OmniSettingsObserver(mHandler); 
+    private SettingsObserver mSettingsObserver = new SettingsObserver(mHandler);
 
     private final BroadcastReceiver mRingerReceiver = new BroadcastReceiver() {
         @Override
@@ -445,9 +429,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 com.android.internal.R.dimen.quick_qs_offset_height) + (mHeaderImageEnabled ?
                 resources.getDimensionPixelSize(R.dimen.qs_header_image_offset) : 0);
 
-        int statusBarBottomMargin = resources.getDimensionPixelSize(
-                R.dimen.qs_header_image_bottom_margin);
-
         mSystemIconsView.getLayoutParams().height = topMargin;
         mSystemIconsView.setLayoutParams(mSystemIconsView.getLayoutParams());
 
@@ -455,15 +436,11 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         if (mQsDisabled) {
             lp.height = topMargin;
         } else {
-	int qsHeight = resources.getDimensionPixelSize(
+        int qsHeight = resources.getDimensionPixelSize(
                     com.android.internal.R.dimen.quick_qs_total_height);
-	if (mHeaderImageEnabled) {
+        if (mHeaderImageEnabled) {
                 qsHeight += resources.getDimensionPixelSize(R.dimen.qs_header_image_offset);
             }
-	    
-	    // always add the margin below the statusbar with or without image
-            qsHeight += statusBarBottomMargin;
-	    
             lp.height = Math.max(getMinimumHeight(), qsHeight);
         }
 
@@ -472,6 +449,17 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         updateStatusIconAlphaAnimator();
         updateHeaderTextContainerAlphaAnimator();
         updatePrivacyChipAlphaAnimator();
+    }
+
+
+     private void updateSettings() {
+        mHeaderImageEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
+        updateResources();
+        updateStatusbarProperties();
+        updateQSBatteryMode();
+        updateSBBatteryStyle();
     }
 
      private void updateQSBatteryMode() {
@@ -753,16 +741,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             lp.leftMargin = sideMargins;
             lp.rightMargin = sideMargins;
         }
-    }
-
-    private void updateSettings() {
-        mHeaderImageEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER, 0,
-                UserHandle.USER_CURRENT) == 1;
-        updateResources();
-        updateStatusbarProperties();
-        updateQSBatteryMode();
-        updateSBBatteryStyle();
     }
 
     // Update color schemes in landscape to use wallpaperTextColor

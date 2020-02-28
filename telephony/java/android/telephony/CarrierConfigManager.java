36d748488ee2c0b8b888da580c06d87363f66669
Sonalika Paul <psonalik@codeaurora.org>
2020-02-28 18:40:53 +0530

    IMS: Add new Carrier Config.
    Add new carrier config for carrier supports
    mutlianchor conference key.
    
    CRs-Fixed: 2556697
    Change-Id: I441db8904f59385f58640bdc16be13e67f10ed62

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -5242,6 +5242,15 @@ public class CarrierConfigManager {
     public static final String KEY_MISSED_INCOMING_CALL_SMS_ORIGINATOR_STRING_ARRAY =
             "missed_incoming_call_sms_originator_string_array";
 
+     /**
+     * Flag indicating whether carrier supports multianchor conference.
+     * In multianchor conference, a participant of a conference can add
+     * other participants to the call using merge button thereby resulting
+     * in a conference with multi anchors.
+     * @hide
+     */
+    public static final String KEY_CARRIER_SUPPORTS_MULTIANCHOR_CONFERENCE =
+            "carrier_supports_multianchor_conference";
 
     /**
      * String array of Apn Type configurations.

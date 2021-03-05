3aacb2509aef1883671b598f7b76e79d7855fb35
ppooopppooo <ppooopppooo@qq.com>
2021-03-05 18:48:13 +0800

    Revert "IS_DEBUGGABLE -> IS_ENG"
    This reverts commit 4d2b4306746b6fba3ae99c5dd505f4e9c57b1017.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -268,7 +268,7 @@ public class QSFactoryImpl implements QSFactory {
         }
 
         // Debug tiles.
-        /*if (Build.IS_ENG) {
+        /*if (Build.IS_DEBUGGABLE) {
             if (tileSpec.equals(GarbageMonitor.MemoryTile.TILE_SPEC)) {
                 return mMemoryTileProvider.get();
             }

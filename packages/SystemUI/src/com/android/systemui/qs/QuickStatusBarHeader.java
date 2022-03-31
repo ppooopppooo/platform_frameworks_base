7ece4978f2856d3d8940979e20f162b5e6b0e353
ppooopppooo <ppooopppooo@qq.com>
2022-03-31 23:56:26 +0800

    Revert "Fix spacing between clock and date"
    This reverts commit 53033a868def4cb4e3133db31a51c587ab403f20.

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -365,25 +365,6 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
 
         updateHeadersPadding();
         updateAnimators();
-
-        updateClockDatePadding();
-    }
-
-    private void updateClockDatePadding() {
-        int startPadding = mContext.getResources()
-                .getDimensionPixelSize(R.dimen.status_bar_left_clock_starting_padding);
-        int endPadding = mContext.getResources()
-                .getDimensionPixelSize(R.dimen.status_bar_left_clock_end_padding);
-        mClockView.setPaddingRelative(
-                startPadding,
-                mClockView.getPaddingTop(),
-                endPadding,
-                mClockView.getPaddingBottom()
-        );
-
-        MarginLayoutParams lp = (MarginLayoutParams) mClockDateView.getLayoutParams();
-        lp.setMarginStart(endPadding);
-        mClockDateView.setLayoutParams(lp);
     }
 
     private void updateAnimators() {

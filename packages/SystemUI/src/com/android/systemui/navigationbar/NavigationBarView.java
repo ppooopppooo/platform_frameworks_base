7d4c7d5940a10c43f1130be30f352ddd931313ff
ppooopppooo <ppooopppooo@qq.com>
2022-11-09 21:44:32 +0800

    Revert "SystemUI: Nuke oriented navbar handle"
    This reverts commit 3bebc14f83c46cff5725887284a2adff73825217.

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -1131,6 +1131,8 @@ public class NavigationBarView extends FrameLayout {
 
         pw.println("    mScreenOn: " + mScreenOn);
 
+
+        dumpButton(pw, "back", getBackButton());
         dumpButton(pw, "home", getHomeButton());
         dumpButton(pw, "handle", getHomeHandle());
         dumpButton(pw, "rcnt", getRecentsButton());

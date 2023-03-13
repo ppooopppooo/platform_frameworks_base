7dc7f12b5a92772ebf89dd1ef1dce7bb018e2e87
Tommy Webb <tommy@calyxinstitute.org>
2023-03-13 16:16:58 -0400

    JobScheduler: Respect allow-in-power-save perm
    Apps like Messaging may be allowlisted to be unaffected by doze mode
    using the allow-in-power-save permission, but prior to this change,
    their jobs are still restricted from running when the device is idle,
    leading to delayed processing. On the other hand, jobs scheduled by
    packages that are user-exempted via the "Unrestricted" app battery
    usage option do not experience such delays. This change causes system-
    exempted packages to be treated the same as user-exempted packages.
    
    Issue: calyxos#1525
    Bug: 223643513
    Test: CtsJobSchedulerTestCases
    Change-Id: I6d95b3940e536e93aae4b2b48706069e964bdb99

--- apex/jobscheduler/service/java/com/android/server/DeviceIdleController.java
+++ apex/jobscheduler/service/java/com/android/server/DeviceIdleController.java
@@ -564,6 +564,12 @@ public class DeviceIdleController extends SystemService
     private final SparseBooleanArray mPowerSaveWhitelistSystemAppIds = new SparseBooleanArray();
 
     /**
+     * Current system app IDs that are in the complete power save white list.  This array can
+     * be shared with others because it will not be modified once set.
+     */
+    private int[] mPowerSaveWhitelistSystemAppIdArray = new int[0];
+
+    /**
      * App IDs that have been white-listed to opt out of power save restrictions, except
      * for device idle modes.
      */
@@ -2217,6 +2223,11 @@ public class DeviceIdleController extends SystemService
             return DeviceIdleController.this.isAppOnWhitelistInternal(appid);
         }
 
+        @Override
+        public int[] getPowerSaveWhitelistSystemAppIds() {
+            return DeviceIdleController.this.getPowerSaveSystemWhitelistAppIds();
+        }
+
         /**
          * Returns the array of app ids whitelisted by user. Take care not to
          * modify this, as it is a reference to the original copy. But the reference
@@ -2399,6 +2410,12 @@ public class DeviceIdleController extends SystemService
         }
     }
 
+    int[] getPowerSaveSystemWhitelistAppIds() {
+        synchronized (this) {
+            return mPowerSaveWhitelistSystemAppIdArray;
+        }
+    }
+
     int[] getPowerSaveWhitelistUserAppIds() {
         synchronized (this) {
             return mPowerSaveWhitelistUserAppIdArray;
@@ -2407,6 +2424,16 @@ public class DeviceIdleController extends SystemService
 
     private static File getSystemDir() {
         return new File(Environment.getDataDirectory(), "system");
+    }
+
+    /** Returns the keys of a SparseBooleanArray, paying no attention to its values. */
+    private static int[] keysToIntArray(final SparseBooleanArray sparseArray) {
+        final int size = sparseArray.size();
+        final int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = sparseArray.keyAt(i);
+        }
+        return array;
     }
 
     @Override
@@ -2445,6 +2472,7 @@ public class DeviceIdleController extends SystemService
                 } catch (PackageManager.NameNotFoundException e) {
                 }
             }
+            mPowerSaveWhitelistSystemAppIdArray = keysToIntArray(mPowerSaveWhitelistSystemAppIds);
 
             mConstants = mInjector.getConstants(this);
 
@@ -4350,6 +4378,7 @@ public class DeviceIdleController extends SystemService
 
     private void passWhiteListsToForceAppStandbyTrackerLocked() {
         mAppStateTracker.setPowerSaveExemptionListAppIds(
+                mPowerSaveWhitelistSystemAppIdArray,
                 mPowerSaveWhitelistExceptIdleAppIdArray,
                 mPowerSaveWhitelistUserAppIdArray,
                 mTempWhitelistAppIdArray);

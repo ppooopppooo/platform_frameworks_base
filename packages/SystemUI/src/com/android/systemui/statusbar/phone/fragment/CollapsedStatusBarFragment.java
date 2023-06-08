3ad715c9559cdac61811eb3d848b09291c1eef21
ppooopppooo <ppooopppooo@qq.com>
2023-06-09 07:39:01 +0800

    Revert "SystemUI: Only apply tuning changes when the fragment is attached"
    This reverts commit 552368f1f62c7ffa57964ce3cbff96dfb90a6d4a.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -30,7 +30,6 @@ import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.Intent;
 import android.database.ContentObserver;
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.os.UserHandle;
@@ -399,11 +398,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     @Override
     public void onTuningChanged(String key, String newValue) {
         boolean wasClockBlacklisted = mIsClockBlacklisted;
-        Context context = getContext();
-        if (context == null)
-            return;
         mIsClockBlacklisted = StatusBarIconController.getIconHideList(
-                context, newValue).contains("clock");
+                getContext(), newValue).contains("clock");
         if (wasClockBlacklisted && !mIsClockBlacklisted) {
             showClock(false);
         }
